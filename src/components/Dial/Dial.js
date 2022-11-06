fa3252bcc093c0efd13cc9c5f3208c825fad1a91
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-06 23:51:42 +0600

    The original state is name as stateful and rest child is stateless

---
+++ src/components/Dial/Dial.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dial = (props) => {
+    return (
+        <div>
+            <p>Dial:{props.dial}</p>
+        </div>
+    );
+};
+
+export default Dial;
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Dial from '../Dial/Dial';
 
 const Display = (props) => {
     return (
         <div>
             <h1>Phone: {props.name}</h1>
             <p>Today's total steps: {props.steps}</p>
+            <Dial dial={props.steps}></Dial>
         </div>
     );
 };
